c60f9bdae1d90ca9976a4a052fcc7f5d77f54334
gaoyuan <gaoyuanyc@163.com>
2026-07-08 00:43:45 +0800

    fix root build.gradle.kts: SQLDelight plugin ID (app.cash.sqldelight)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
 plugins {
     id("org.jetbrains.kotlin.multiplatform") version "2.3.21" apply false
     id("com.android.library") version "8.13.2" apply false
-    id("com.squareup.sqldelight") version "2.0.2" apply false
+    id("app.cash.sqldelight") version "2.0.2" apply false
 }
 
 tasks.register("clean", Delete) {
